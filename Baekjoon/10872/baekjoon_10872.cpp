3a0747f16788d7ad3fab4da2638fbfd451502e9f
Jung Eun Kim <starfire199471@gmail.com>
2023-07-23 23:10:39 +0900

    Add solution for problem 10872 of Baekjoon's programming judge.

---
+++ Baekjoon/10872/baekjoon_10872.cpp
@@ -0,0 +1,18 @@
+////BAEKJOON PROGRAMMING JUDGE 10872
+//
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(void)
+//{
+//	unsigned long long factorial = 1;	//factorial number increases rapidly
+//	int n;
+//
+//	cin >> n;
+//	for (int i = 1; i <= n; i++)
+//		factorial *= i;	//calculate factorial
+//	cout << factorial << endl;
+//
+//	return 0;
+//}
